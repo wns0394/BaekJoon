522cdb4bd1538de2eceb4fb741f534a9ced0542a
wns0394 <97592486+wns0394@users.noreply.github.com>
2023-07-17 14:14:32 +0900

    [Gold IV] Title: 플로이드, Time: 256 ms, Memory: 116232 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,26 @@
+inf = int(1e9)
+
+n = int(input())
+m = int(input())
+
+arr = [[inf] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    arr[i][i] = 0
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if arr[a][b] > c:
+        arr[a][b] = c
+
+for i in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            arr[a][b] = min(arr[a][b], arr[a][i] + arr[i][b])
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if arr[i][j] == inf:
+            print(0, end=' ')
+        else:
+            print(arr[i][j], end=' ')
+    print()
