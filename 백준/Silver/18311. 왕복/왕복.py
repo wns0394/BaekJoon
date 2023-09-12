f3c9850989b1c3e31105f3583233dddfa6efc4f2
wns0394 <97592486+wns0394@users.noreply.github.com>
2023-09-12 14:38:15 +0900

    [Silver V] Title: 왕복, Time: 148 ms, Memory: 125976 KB -BaekjoonHub

---
+++ 백준/Silver/18311. 왕복/왕복.py
@@ -0,0 +1,16 @@
+n,k = map(int,input().split())
+
+arr = list(map(int,input().split()))
+
+for i in range(n):
+    k -= arr[i]
+    if k < 0:
+        print(i+1)
+        break
+
+if k >= 0:
+    for i in range(n-1,-1,-1):
+        k -= arr[i]
+        if k < 0:
+            print(i+1)
+            break
