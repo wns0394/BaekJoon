2cb0a246816b79e377a96297b4bda76bfda25d8a
wns0394 <97592486+wns0394@users.noreply.github.com>
2023-09-13 21:25:32 +0900

    [level 2] Title: 피보나치 수, Time: 20.13 ms, Memory: 13.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    d = [0] * (n+1)
+    d[0] = 0
+    d[1] = 1
+    for i in range(2,n+1):
+        d[i] = (d[i-1] + d[i-2]) % 1234567
+
+    answer = d[n]
+    return answer
