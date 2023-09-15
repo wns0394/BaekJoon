87b3da772e2fb32326c351b78fa036d8fdea3654
wns0394 <97592486+wns0394@users.noreply.github.com>
2023-09-15 15:08:56 +0900

    [Gold V] Title: 시간 관리하기, Time: 144 ms, Memory: 114844 KB -BaekjoonHub

---
+++ 백준/Gold/6068. 시간 관리하기/시간 관리하기.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+arr = []
+
+for _ in range(n):
+    t,s = map(int,input().split())
+
+    arr.append((t,s))
+
+arr.sort(key= lambda x: x[1])
+result = 1000001
+pre = 0
+for i in range(n):
+    if arr[i][1] - (pre+arr[i][0]) >= 0:
+        pre += arr[i][0]
+        result = min(result,arr[i][1] -pre)
+    else:
+        result = -1
+        break
+print(result)
