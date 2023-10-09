26eb4e4d55b2be7c31ac5eef0e2ec1dd6b8eed3d
wns0394 <97592486+wns0394@users.noreply.github.com>
2023-10-09 15:11:51 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,29 +1,27 @@
 def solution(s):
     answer = 1001
-    # 압축은 절반까지만 될것이다
-    result = []
-    # 자르는 개수 i
     
-    if len(s) == 1:
-        answer = 1
-    else:
-        for i in range(1,len(s)//2+1):
-            x = ""
-            y = s[0:i]
-            count = 1
-
-            for j in range(i, len(s) + i, i):
-                if y == s[j:j+i]:
-                    count += 1
-                else:
-                    if count != 1:
-                        x = x + str(count) + y
-                    else:
-                        x = x + y
-
-                    y = s[j:j+i]
-                    count = 1
-            result.append(len(x))
-        m = min(result)
-        answer = min(answer,m)
+    # 압축 제일 많이 해도 절반 이므로 
+    # i는 압축 길이
+    for i in range(1, len(s) + 1):
+        x = s[0:i]
+        result = ''
+        num = 1
+        # i부터 끝까지 압축길이 만큼 조사 
+        for j in range(i, len(s), i):
+            # 압축한거랑 같으면 num 증가
+            if x == s[j:j+i]:
+                num += 1
+            # 다르면 
+            else:
+                if num != 1:
+                    result += str(num)  
+                result += x
+                x = s[j:j+i]
+                num = 1
+        if x:
+            if num != 1:
+                result += str(num)
+            result += x
+        answer = min(answer, len(result))
     return answer
