e6bd4fcb9afa78a58ee3f154d95e9bb38cb675e9
wns0394 <97592486+wns0394@users.noreply.github.com>
2024-01-21 23:19:07 +0900

    [Gold V] Title: 퇴사 2, Time: 876 ms, Memory: 280500 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,22 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
 
-arr = [[0, 0]]
+arr = []
 
 for _ in range(n):
     t, p = map(int, input().split())
     arr.append([t, p])
-    
+
 d = [0] * (n + 1)
 
-for i in range(1, n+1):
-    # i날의 최대는 그 전날이랑 같거나 클테니
-    d[i] = max(d[i - 1], d[i])
-    # i날에 일 시작한다고 생각하면
-    # (i + arr[i][0] - 1) 일날 끝난다.
-    # 그러므로 i + arr[i][0] - 1 <= n+1을 만족하면
-    if i + arr[i][0] - 1 <= n:
-        # 끝나는 날짜의 최대값은
-        # 끝나는 날짜의 최대 와 i-1일에서최대값 + i일만큼 일하는 금액
-        d[i + arr[i][0] - 1] = max(d[i + arr[i][0] - 1], d[i-1] + arr[i][1])
+for i in range(1, n + 1):
+    # i번째 요일의 최대값은 그전날보다 무조건 크거나 같다.
+    d[i] = max(d[i], d[i - 1])
+
+    # i번째 요일에 일을 한다면
+    # i + t_i - 1 일에 끝날테니 그게 n일 안에 존재하고
+    if i + arr[i-1][0] - 1 <= n:
+        # i + arr[i-1][0] - 1 요일의 최대값은
+        # 그 당일이 최대이거나 아니라면
+        # i-1번째 일의 최대에서 i번째 요일의 받은 금액을 더한 것과 비교
+        d[i + arr[i-1][0] - 1] = max(d[i + arr[i-1][0] - 1], d[i-1] + arr[i-1][1])
 
 print(d[-1])
