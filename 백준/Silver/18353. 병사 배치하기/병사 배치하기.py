dc0d2cb8e607e942e1b827a200fc9a54abc270c9
wns0394 <97592486+wns0394@users.noreply.github.com>
2024-01-23 15:22:45 +0900

    [Silver II] Title: 병사 배치하기, Time: 132 ms, Memory: 110780 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -1,13 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
 from bisect import bisect_left
 
 n = int(input())
 
-arr = list(map(int,input().split()))
+arr = list(map(int, input().split()))
+
 new = list(reversed(arr))
 
-d = []
+# 감소하는 부분수열 만들기
 
-d.append(new[0])
+d = [new[0]]
 
 for i in range(n):
     if new[i] > d[-1]:
@@ -15,5 +20,5 @@ for i in range(n):
     else:
         idx = bisect_left(d, new[i])
         d[idx] = new[i]
-
+    # print(d, i)
 print(n-len(d))
