b34eec387e47f9c4d2ddb4ad775ca6dce9e99214
wns0394 <97592486+wns0394@users.noreply.github.com>
2024-08-06 11:49:05 +0900

    [Silver III] Title: N과 M (1), Time: 2408 ms, Memory: 13536 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+const input =
+    process.platform === "linux"
+        ? fs.readFileSync("/dev/stdin").toString().split("\n")
+        : fs.readFileSync("example.txt").toString().split("\r\n");
+
+
+const [n,m] = input[0].split(' ').map((e)=> parseInt(e))
+
+
+let result = []
+let visited = Array(n+1).fill(0)
+const dfs = (x) => {
+
+    if (x == m) {
+        console.log(result.join(" "))
+        return
+    }
+
+    for (let i=1; i< n+1; i++) {
+        if (visited[i] == 0) {
+            result.push(i)
+            visited[i] = 1
+            dfs(x+1)
+            visited[i] = 0
+            result.pop()
+        }
+        
+    }
+}
+
+dfs(0)
