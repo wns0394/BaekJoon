89b8983b45c24cda2bb2dcd7b4064bad3564a7aa
wns0394 <97592486+wns0394@users.noreply.github.com>
2024-09-26 23:32:20 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.70 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public List solution(int n, int k) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 1; k*i <= n; i++) {
+            answer.add(k*i);
+        }
+        
+        return answer;
+    }
+}
